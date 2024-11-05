57e3461e5cbbe91f55b457aa1e9fc3bf43e3415f
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-05 14:09:28 +0200

    add icons

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import {AiOutlineShopping} from "react-icons/ai";
+import {AiOutlineHighlight, AiOutlineShopping} from "react-icons/ai";
 
 export const Overlay = () => {
-  return <Intro />;
+  // return <Intro />;
+  return <Customize />;
 };
 
 const Intro = () => {
   return (
-    <div className="container">
-      <header>
+    <div className="fixed inset-0 text-white p-4">
+      <header className="flex justify-between">
         <div className="">logo</div>
         <AiOutlineShopping size="3rem" />
       </header>
@@ -16,9 +17,11 @@ const Intro = () => {
       <section key="main">
         <div className="section--container">
           <div>
-            <h1>LET'S DO IT.</h1>
+            <h1 className="text-8xl lg:text-9xl font-black tracking-tighter italic w-1/3 leading-none max-w-80">
+              LET'S DO IT.
+            </h1>
           </div>
-          <div className="support--content">
+          <div className="support--content w-1/3 max-w-96">
             <div>
               <p>
                 Create your unique and exclusive shirt with our brand-new 3D
@@ -26,11 +29,38 @@ const Intro = () => {
                 <strong>Unleash your imagination</strong> and define your own
                 style.
               </p>
-              <button>Start</button>
+              <button className="mt-8 bg-black flex items-center gap-2 px-4 py-2 rounded-full">
+                CUSTOMIZE IT
+                <AiOutlineHighlight size="1.3rem" />
+              </button>
             </div>
           </div>
         </div>
       </section>
+    </div>
+  );
+};
+
+const Customize = () => {
+  const colors = [
+    "#ccc",
+    "#efbd4d",
+    "#80c670",
+    "#726de8",
+    "#ef674d",
+    "#353934",
+  ];
+  return (
+    <div className="fixed inset-0 p-4">
+      <div className="flex gap-4 absolute bottom-4 left-1/2 -translate-x-1/2">
+        {colors.map((color) => (
+          <div
+            key={color}
+            className="w-16 h-16 rounded-full border-2 border-white"
+            style={{backgroundColor: color}}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
