6e719fd7db5f9428ce2f91918ff2d2e6d4220b10
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-06 11:54:21 +0200

    refactoring

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -9,47 +9,48 @@ import {useSnapshot} from "valtio";
 import {state} from "../storage";
 
 export const Overlay = () => {
-  const snap = useSnapshot(state);
-  return snap.intro ? <Intro /> : <Customize />;
+  // const snap = useSnapshot(state);
+  // return snap.intro ? <Intro /> : <Customize />;
+  return <Customize />;
 };
 
-const Intro = () => {
-  return (
-    <div className="fixed inset-0 text-white p-4">
-      <header className="flex justify-between">
-        <div className="">logo</div>
-        <AiOutlineShopping size="3rem" />
-      </header>
+// const Intro = () => {
+//   return (
+//     <div className="fixed inset-0 text-white p-4">
+//       <header className="flex justify-between">
+//         <div className="">logo</div>
+//         <AiOutlineShopping size="3rem" />
+//       </header>
 
-      <section key="main">
-        <div className="section--container">
-          <div>
-            <h1 className="text-8xl lg:text-9xl font-black tracking-tighter italic w-1/3 leading-none max-w-80">
-              LET'S DO IT.
-            </h1>
-          </div>
-          <div className="support--content w-1/3 max-w-96">
-            <div>
-              <p>
-                Create your unique and exclusive shirt with our brand-new 3D
-                customization tool.
-                <strong>Unleash your imagination</strong> and define your own
-                style.
-              </p>
-              <button
-                className="mt-8 bg-black flex items-center gap-2 px-4 py-2 rounded-full"
-                onClick={() => (state.intro = false)}
-              >
-                CUSTOMIZE IT
-                <AiOutlineHighlight size="1.3rem" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+//       <section key="main">
+//         <div className="section--container">
+//           <div>
+//             <h1 className="text-8xl lg:text-9xl font-black tracking-tighter italic w-1/3 leading-none max-w-80">
+//               LET'S DO IT.
+//             </h1>
+//           </div>
+//           <div className="support--content w-1/3 max-w-96">
+//             <div>
+//               <p>
+//                 Create your unique and exclusive shirt with our brand-new 3D
+//                 customization tool.
+//                 <strong>Unleash your imagination</strong> and define your own
+//                 style.
+//               </p>
+//               <button
+//                 className="mt-8 bg-black flex items-center gap-2 px-4 py-2 rounded-full"
+//                 onClick={() => (state.intro = false)}
+//               >
+//                 CUSTOMIZE IT
+//                 <AiOutlineHighlight size="1.3rem" />
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
 
 const Customize = () => {
   const colors = [
@@ -79,22 +80,13 @@ const Customize = () => {
         {decals.map((decal) => (
           <div
             key={decal}
-            className="w-16 h-16"
+            className="w-16 h-16 bg-white rounded-full flex items-center justify-center p-1 bg-opacity-15"
             onClick={() => (state.decal = decal)}
           >
             <img src={`/${decal}-icon.png`} alt={decal} />
           </div>
         ))}
       </div>
-
-      <button
-        className=" text-white flex items-center gap-2 px-4 py-2 rounded-full absolute top-4 left-4"
-        onClick={() => (state.intro = true)}
-        style={{backgroundColor: state.color}}
-      >
-        GO BACK
-        <AiOutlineArrowLeft size="1.3rem" />
-      </button>
     </div>
   );
 };
